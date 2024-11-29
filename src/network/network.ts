8bfd1c80b72f673c8b8adbde0ce55ebb4ba4c1f5
Yehuda Briskman <yr0556772363@gmail.com>
2024-11-29 10:54:35 +0200

    update networking

--- src/network/network.ts
+++ src/network/network.ts
@@ -5,8 +5,8 @@ import { algoQueries } from './algo_querues';
 export const ALGO_SERVER_IP = "172.17.0.2";
 export const ALGO_SERVER_PORT = 5003;
 
-export const MONGO_SERVER_IP = '192.168.68.53';
-export const MONGO_SERVER_PORT = 5002;
+export const MONGO_SERVER_IP = '127.0.0.1';
+export const MONGO_SERVER_PORT = 5000;
 
 export const MongoClient = new ApolloClient({
     link: new HttpLink({ uri: `http://${MONGO_SERVER_IP}:${MONGO_SERVER_PORT}/graphql` }),
