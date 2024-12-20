2d3bb77624b3ce67188b6ce65d05fe31856613e5
Yehuda Briskman <yr0556772363@gmail.com>
2024-12-20 09:37:24 +0200

    update manual show

--- src/components/creationMode/creationModeMenu/CreationModeMenu.tsx
+++ src/components/creationMode/creationModeMenu/CreationModeMenu.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import AddingModeEnum from "../../../utils/addingModeEnum";
-// import Switch from '../manualResponse/switch';
 import classes from "../manualResponse/switch.module.css"
 import ButtonsMenu from './buttonsMenu/ButtonsMenu';
-// import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
 import SidebarHeader from '../../sidebarHeader/SidebarHeader';
-// import { switchToUpSideDown } from '../../../store/slices/switchSlice';
 import { SetProgressType, progressType } from '../../../pages/creationMode/CreationMode';
 import { useSelector } from 'react-redux';
 
@@ -25,18 +22,10 @@ export default function CreationModeMenu({
 }: CreationModeMenuProps,) {
   const switchState = useSelector((root: RootState) => root.switch.switchHolder);
 
-  // const dispatch = useDispatch();
-
-  // const toggleSwitch = () => {
-  //   dispatch(switchToUpSideDown())
-  // }
-
   return (
     <>
       <SidebarHeader name="Gondola" />
       <div className={classes.switchAIContainer}>
-        {/* <span className={`${classes.defaultTitle} ${classes.itemMenu}`}>AI</span>
-        <Switch typeMission={switchState} toggleSwitch={toggleSwitch} /> */}
         <span className={`${classes.trueTitle} ${classes.itemMenu}`}>Manual</span>
       </div>
       <ButtonsMenu progress={progress} setProgress={setProgress} setAddingMode={setAddingMode} typeMission={switchState} />
