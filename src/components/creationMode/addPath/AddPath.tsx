e4d4e53d53f2cfb9147811d0e3273273b64bbcf8
Yehuda Briskman <yr0556772363@gmail.com>
2024-12-20 11:19:33 +0200

    update save paths

--- src/components/creationMode/addPath/AddPath.tsx
+++ src/components/creationMode/addPath/AddPath.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Path } from '../../../gondola_types/basicElements';
 import { LeafletMouseEvent } from 'leaflet';
 import { useMapEvent } from 'react-leaflet';
 import DisplayPath from '../../map/DisplayPath';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../../store/store';
+import { useDispatch } from 'react-redux';
 import { clearExitPath, setExitPath } from '../../../store/slices/exitPathSlice';
 import { setEntryPath, clearEntryPath } from '../../../store/slices/entryPathSlice';
 import { getFixedPoint } from '../../../utils/general';
@@ -18,14 +17,16 @@ type AddPathProps = {
 }
 
 const AddPath = ({ path, setAddingMode }: AddPathProps) => {
-    const exitPoints = useSelector((e: RootState) => e.exitPath.pathsHolder);
-    const entryPoints = useSelector((e: RootState) => e.entryPath.pathsHolder);
     const dispatch = useDispatch();
     const [temporaryPath, setTemporaryPath] = useState<Partial<Path>>({});
+    const [isMouseMovementEnabled, setIsMouseMovementEnabled] = useState(false);
+
+    useEffect(() => {
+        console.log('Temporary Path updated:', temporaryPath);
+    }, [temporaryPath]);
 
     const onCancel = () => {
         if (window.confirm(`Are you sure you want to delete the ${path} path?`)) {
-            // בדיקה אם מדובר ב-entry או exit והפעלת הפעולה המתאימה
             if (path === "entry") {
                 dispatch(clearEntryPath());
             } else {
@@ -36,13 +37,17 @@ const AddPath = ({ path, setAddingMode }: AddPathProps) => {
     }
 
     const onSave = () => {
+        console.log("save");
+
         const { endPoint, startPoint } = temporaryPath;
         if (endPoint && startPoint) {
-            // בדיקה אם מדובר ב-entry או exit והפעלת הפעולה המתאימה
             if (path === "entry") {
                 dispatch(setEntryPath({ path: { endPoint, startPoint } }));
+                console.log("entry");
+
             } else {
                 dispatch(setExitPath({ path: { endPoint, startPoint } }));
+                console.log("exit");
             }
         }
         setAddingMode(AddingModeEnum.INITIAL_STATE);
@@ -50,26 +55,30 @@ const AddPath = ({ path, setAddingMode }: AddPathProps) => {
 
     const handleMapClick = (e: LeafletMouseEvent) => {
         const { lat, lng } = getFixedPoint(e);
-        
-        // אם יש כבר נתיב (entry או exit), נגיב בהתאם
+
         if (temporaryPath.startPoint === undefined) {
-            setTemporaryPath({ startPoint: { Latitude: lat, Longitude: lng } });
-        } else if (temporaryPath.endPoint === undefined) {
+            setTemporaryPath({ startPoint: { Latitude: lat, Longitude: lng }, endPoint: undefined });
+            setIsMouseMovementEnabled(true);
+        } else if (isMouseMovementEnabled) {
             setTemporaryPath(p => ({ ...p, endPoint: { Latitude: lat, Longitude: lng } }));
+            setIsMouseMovementEnabled(false);
         } else {
-            // שמירה של הנתיב המתואם בהתאם ל-entry או exit
             if (path === "entry") {
                 dispatch(setEntryPath({ path: temporaryPath as Path }));
             } else {
                 dispatch(setExitPath({ path: temporaryPath as Path }));
             }
             setTemporaryPath({});
+            setIsMouseMovementEnabled(false);
         }
     }
 
     const handleMapMouseMovement = (e: LeafletMouseEvent) => {
+        if (!isMouseMovementEnabled) return;
+
         const { lat, lng } = getFixedPoint(e);
-        if (temporaryPath.startPoint && temporaryPath.endPoint === undefined) {
+
+        if (temporaryPath.startPoint && isMouseMovementEnabled) {
             setTemporaryPath(p => ({ ...p, endPoint: { Latitude: lat, Longitude: lng } }));
         }
     }
