73d7450bf62d60408c93ae9f3e3e346f99ef47f2
Yehuda Briskman <yr0556772363@gmail.com>
2025-02-17 15:52:28 +0200

    update

--- src/network/network.ts
+++ src/network/network.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { mongoQueries } from './mongo_queries';
 
-export const MONGO_SERVER_IP = '127.0.0.1';
-export const MONGO_SERVER_PORT = 5000;
+export const MONGO_SERVER_IP = 'flask-app'; // שם השירות מ-docker-compose
+export const MONGO_SERVER_PORT = 5002;
 
 export const MongoClient = new ApolloClient({
     link: new HttpLink({ uri: `http://${MONGO_SERVER_IP}:${MONGO_SERVER_PORT}/graphql` }),
